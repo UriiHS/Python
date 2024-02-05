9718a424772f84be0801d6ce65cea621f7d90e27
UriiHS <ohs-@hotmail.com>
2024-02-05 16:02:09 +0100

    Primera prueba

---
+++ Ejercicios1.py
@@ -0,0 +1,250 @@
+## Exercise 1: Calculate the multiplication and sum of two numbers
+#Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+def exercise1 (num1, num2):
+    result = num1*num2
+    if( result <= 1000):
+        return result
+    else:
+        return num1+num2
+    
+final1 = exercise1(20,30)
+#print("The result is ",final1)
+
+final2 = exercise1(40,30)
+#print("The result is ",final2)
+
+##Exercise 2: Print the sum of the current number and the previous number
+#Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+#Mi solución (He echo un poco extra para que sea una función y puedas poner el rango que quieras.)
+def exercise2 (rango):
+    i = 0
+    while i <= rango:
+        previus_num = i-1
+        if (previus_num < 0):
+            previus_num = 0
+        print("Current Number ",i," Previous Number ",previus_num," Sum: ",i+previus_num )
+        i += 1
+# print(exercise2(10))
+#Solución de la página
+
+def exercise2_2 (rango):
+    previous_num = 0
+    for i in range(1, rango):                  #Utiliza la funcion range()
+        x_sum = previous_num + i
+        print("Current Number", i, "Previous Number ", previous_num, " Sum: ", x_sum)
+        previous_num = i
+
+#print(exercise2_2(11))
+
+## Exercise 3: Print characters from a string that are present at an even index number
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+#texto = input("Escribe cualquier palabra: ")
+
+#long_texto = len(texto)
+#def exercise3 (texto):
+#    for i in range(0, long_texto, 2):
+#        print("Index[",i,"]", texto[i])
+
+
+## Exercise 4: Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old
+
+import datetime
+
+def exercise4 ():
+    nombre = input("Como te llamas: ")
+    edad = int(input("Escribe tu edad: "))
+    año_actual = datetime.datetime.now().year
+    print(nombre, "cumplirás los 100 años el año: ", año_actual + 100 - edad)
+
+#exercise4()
+
+#Vamos a hacer el ejercicio 4, pero esta vez pedirás la fecha de nacimiento y lo calculará
+    
+def exercise4_2():
+    nombre = input("Como te llamas: ")
+    fecha_nacimiento = input("Escribe la fecha de nacimiento: (DD/MM/AAAA)")
+    # Tratammos lo que ha escrito el cliente
+    if (fecha_nacimiento.count("/") == 2):
+        fecha_val = fecha_nacimiento.split("/")
+        dia_cliente = int(fecha_val[0])
+        mes_cliente = int(fecha_val[1])
+        año_cliente = int(fecha_val[2])
+    else:
+        print("Loco, sigue las instrucciones T.T")
+        exercise4_2()
+
+    #Obtenemos los valores de la fecha actual
+    fecha_actual = datetime.datetime.now()
+    año_actual = fecha_actual.year
+    mes_actual = fecha_actual.month
+    dia_actual = fecha_actual.day
+
+    def fecha_no_valida():
+        print(" Wooah, aun no has nacido, eres del futuro lol (espabila loquete)")
+        exercise4_2()
+
+    if (año_cliente > año_actual):
+        fecha_no_valida()
+    elif(año_cliente == año_actual):
+        if (mes_cliente > mes_actual):
+            fecha_no_valida()
+        elif (mes_cliente == mes_actual):
+            if(dia_cliente >= dia_actual):
+                fecha_no_valida()
+            else:
+                dia_restante = 30 - dia_actual + dia_cliente
+                meses_restantes = 11
+                años_restantes = 99
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años") 
+        else:    
+            if(dia_cliente >= dia_actual):
+                dia_restante = dia_cliente- dia_actual 
+                meses_restantes = 12 - mes_actual + mes_cliente
+                años_restantes = 99
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años")
+            else:
+                dia_restante = 30 - dia_actual + dia_cliente
+                meses_restantes = 12 - mes_actual + mes_cliente
+                años_restantes = 99
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años") 
+    else:
+        if (mes_cliente > mes_actual):
+            if (dia_cliente >= dia_actual):
+                dia_restante = dia_cliente - dia_actual
+                meses_restantes = mes_cliente - mes_actual
+                años_restantes = año_actual - año_cliente + 100
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años")
+            else:
+                dia_restante = 30 - dia_actual + dia_cliente
+                meses_restantes = mes_cliente - mes_actual -1
+                años_restantes = año_actual - año_cliente + 100
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años")
+        elif (mes_cliente == mes_actual):
+            if(dia_cliente >= dia_actual):
+                dia_restante = dia_cliente - dia_actual
+                meses_restantes = mes_cliente - mes_actual
+                años_restantes = año_actual - año_cliente + 99
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años")
+            else:
+                dia_restante = 30 - dia_actual + dia_cliente
+                meses_restantes = mes_cliente - mes_actual -1
+                años_restantes = año_actual - año_cliente + 100
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años")
+        else:    
+            if(dia_cliente >= dia_actual):
+                dia_restante = dia_cliente - dia_actual
+                meses_restantes =12 + mes_cliente - mes_actual
+                años_restantes = año_actual - año_cliente + 100
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años")
+            else:
+                dia_restante = 30 - dia_actual + dia_cliente
+                meses_restantes =12 + mes_cliente - mes_actual
+                años_restantes = año_actual - año_cliente + 100
+                print("Te quedan ", dia_restante , "Dias ",meses_restantes," Meses",años_restantes," Años, para cumplir 100 años")
+                
+
+# exercise4_2()
+                
+##Exercise 5 Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
+#Extra 2    Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). 
+            #If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+                
+def exercise5():
+    numero_cliente = input("Escribe un número: ")
+    numero_cliente = int(numero_cliente)
+    par_or_not = numero_cliente % 2
+
+    if(par_or_not >= 1):
+        print("El número que has escrito es impar")
+    else:
+        print("El número que has escrito es par")
+
+#exercise5()
+
+#Extra 1    If the number is a multiple of 4, print out a different message.
+
+def exercise5_1():
+    numero_cliente = input("Escribe un número: ")
+    numero_cliente = int(numero_cliente)
+    par_or_not = numero_cliente % 2
+    multiple_4 = numero_cliente % 4
+
+  
+    if(multiple_4>=1):
+        if(par_or_not >= 1):
+            print("El número que has escrito es impar")
+        else:
+            print("El número que has escrito es par")
+    else:
+        print("El número que has escrito es par y además es multiple de 4")
+   
+
+#exercise5_1()
+
+#Extra 2    Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). 
+            #If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+def exercise5_2():
+    numero_cliente = input("Escribe un número: ")
+    numero_check = input("Escribe otro número: ")
+    numero_cliente = int(numero_cliente)
+    numero_check = int(numero_check)
+
+    resultado = numero_cliente % numero_check
+    total = numero_cliente / numero_check
+
+    if(resultado >= 1):
+        print("El número: ",numero_cliente," que has puesto, no es divisible por: ",numero_check)
+    else:
+        print("El número: ",numero_cliente," que has puesto, es divisible por: ",numero_check, " y da como resultado: ",total)
+
+#exercise5_2()
+        
+## Exercise 6   Take a list and write a program that prints out all the elements of the list that are less than 5.
+
+def exercise6(num):
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    i = 0
+
+    while i < len(a):
+        if(a[i]<num):
+            print (a[i])
+        i += 1
+
+#exercise6(15)
+
+#Extra 1       Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+def exercise6_1(num):
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    i = 0
+    b = []
+
+    while i < len(a):
+        if(a[i]<num):
+            b.append(a[i]) 
+        i += 1
+
+    print(b)
+
+#exercise6_1(15)
+
+#Extra 2       Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+def exercise6_2():
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    i = 0
+    b = []
+
+    num = int(input("Escribe un valor: "))
+
+    while i < len(a):
+        if(a[i]<num):
+            b.append(a[i]) 
+        i += 1
+
+    print(b)
+
+#exercise6_2()
---
+++ Python_1.py
@@ -0,0 +1,104 @@
+### Variables
+#a = "El resultado es: "                  
+#b = 12                                   
+#c = -1                                  
+#print(a,b+c)                            Resultado para mostrar varias variables
+#print(2*8)                              Se pueden hacer operaciones con los numeros sin declararlo como varaible
+#print("La division es: ",round(8/3))    round() sirve para redondear decimales
+
+### Arrays or List
+#array = [a, b, "tercer valor"]          Asi se crea una array/list
+#print(array)                            Asi se muestra una array
+#print(array[1])                         Asi se muestra un valor en una posicion x en una array/list
+#array.append("nuevo valor añadido")     El append() sirve para añadir un nuevo valor a la array/list
+#array.pop()                             El pop() sirve para eliminar el último valor añadido a la array/list
+#array.pop(1)                            Borra el valor que este en la posición indicada en el pop()   
+#print(len(array))                       len() muestra la longitud de lo que haya dentro varaible, string, etc...
+
+## Mini ejercicio, crea una array/list con tres nombres y guarda el segundo valor de esta lista en una variable
+#lista_nombres = ["Oriol","Ganzalo","Rubia"]
+#nombre_a_guardar = lista_nombres[1]
+#print(nombre_a_guardar)
+
+
+### Dictionaries
+#dict_a = {"a":"Oro","b":"Plata","c":"Bronce"}      Crea un dictionary, es similar a las arrays/list pero con el primer puesyo es la key y el segundo es el valor.
+#print(dict_a)                                      Muestra todo el dictionary
+#print(dict_a["a"])                                 Ponemos la key para que nos muestre su valor
+
+### Tuples
+#tup = ("a","b")                                    Sirve para guardar valores, como las arrays/list y los dictionaries
+#print(tup[1])                                      Así se muestran
+
+### Loops
+#bolsa_compra = ["pasta","arroz","carne","pescado"]
+#i = 0
+
+## Bucle FOR                                        Bucle FOR que muestra todos los objetos en una lista/array  (por cada)
+#for item in bolsa_compra:                          
+#    print(item)
+
+#for item in bolsa_compra:                          Bucle FORque cuenta cuanto objetos hay en la lista/array
+#    i = i + 1
+#    print(i)
+
+#for item in bolsa_compra:                          Bucle que utiliza una condicion
+#    i = i + 1
+#    if item == "carne":
+#        print("Correcto, tenemos carne y está en la posición:", i)
+
+##Bucle while                                       Bucle WHILE hace repeticiones de lo que haya dentro hasta que se deje de cumplir la condicion  (mientras)              
+#while i <= 10:
+#    print(i)
+#    i = i +1
+
+### Conditionals
+a ="a"
+b = "b"
+## Boolean values (True or False)
+## El símgolo = sirve para asiganr el valor a una variable 
+## El símbolo == o escribir != o poner "is" sirve para comprar dos valores
+#print(a == b)
+#print(a is b)
+
+## El not delante de una variable hace que sea negativa
+#print(not a == b)
+#print(a != b)
+
+## Luego estan las valores matematicos de mas grande o mas pequeño igual que ... >,<,>=,<=
+##El else if se escribe "elif" y el else es else
+
+### Functions siempre con ()
+items = ["Oriol",26,09.02,"Mataró"]
+## Funcion de ordenar .sort() Sirve para Strings (entre ellas) y luego con los int y float.
+#items_Str = ["Herrero","García","Sobrino","Fuentes"]
+# Por defecto lo ordena de forma ascendiente
+#items_Str.sort()
+# también se pueden usar keys para ordenar de diferente manera
+#items_Str.sort(key=str.lower, reverse=True)
+#Para no modificar la la variable con la lista, podemos usar sorted()
+#new_items = sorted(items_Str, key=str.lower, reverse=True)
+
+## Funcion sum(), sirve para sumar integers y floats conjuntos
+numeros = [234,2344.234,768,4.67]
+#print(sum(numeros))
+
+## Para crear una funcion utilizamos "def" para definirla
+#def exercise1 (num1, num2):
+#    result = num1*num2
+#    if( result <= 1000):
+#        return result
+#    else:
+#        return num1+num2
+#final1 = exercise1(20,30)
+#print("The result is ",final1)
+
+#range(), sirve para establecer un rango segun el valor que le pongas
+#for i in range(6):
+    #print(i)
+#input(), sirve para guardar valor que ponga el usaurio
+#name = input("Enter Employee Name: ")
+#print(name)
+
+
+
